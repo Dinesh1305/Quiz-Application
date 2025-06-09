743f07958d7241297e6ff7c75efb6366c81a702c
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-06-09 08:46:54 +0530

    Submission class was created

---
+++ src/main/java/com/demo/model/Submission.java
@@ -0,0 +1,28 @@
+package com.demo.model;
+
+public class Submission {
+private Integer id;
+private String question;
+private String rightanwer;
+
+
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getQuestion() {
+	return question;
+}
+public void setQuestion(String question) {
+	this.question = question;
+}
+public String getRightanwer() {
+	return rightanwer;
+}
+public void setRightanwer(String rightanwer) {
+	this.rightanwer = rightanwer;
+}
+
+}
