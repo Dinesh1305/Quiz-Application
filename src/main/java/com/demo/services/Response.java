4451b9d5ee56adb270c43a1eece79a5b6e9eda35
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-06-09 09:01:55 +0530

    SubmissionService class are completed

---
+++ src/main/java/com/demo/services/Response.java
@@ -0,0 +1,21 @@
+package com.demo.services;
+
+public class Response {
+private Integer id;
+private String answer_selected;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getAnswer_selected() {
+	return answer_selected;
+}
+public void setAnswer_selected(String answer_selected) {
+	this.answer_selected = answer_selected;
+}
+
+
+
+}
--- src/main/java/com/demo/services/SubmissionService.java
+++ src/main/java/com/demo/services/SubmissionService.java
@@ -1,5 +1,41 @@
 package com.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.model.Questions;
+import com.demo.model.Quiz;
+import com.demo.repo.QuizRepo;
+
+@Service
 public class SubmissionService {
 
+	@Autowired
+	QuizRepo qr;
+	public Integer Caculate(Integer id,List<Response>res)
+	{
+	Optional<Quiz> q=	qr.findById(id);
+	
+	
+	List<Questions>question=q.get().getQuestion();
+	
+	
+	int count=0;
+	int right_answer=0;
+	
+	for(Response r:res)
+	{
+		if(r.getAnswer_selected().equals(question.get(count).getRightanswer()))
+			right_answer++;
+		count++;
+	}
+	
+	
+	return right_answer;
+	}
+	
+	
 }
