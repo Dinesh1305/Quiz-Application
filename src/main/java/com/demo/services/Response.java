bae02a67cc8a95b0020f16ab9fb76e2f96804c7a
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-06-09 09:02:53 +0530

    Response class are changed

--- src/main/java/com/demo/services/Response.java
+++ src/main/java/com/demo/services/Response.java
@@ -1,14 +1,9 @@
 package com.demo.services;
 
 public class Response {
-private Integer id;
+
 private String answer_selected;
-public Integer getId() {
-	return id;
-}
-public void setId(Integer id) {
-	this.id = id;
-}
+
 public String getAnswer_selected() {
 	return answer_selected;
 }
